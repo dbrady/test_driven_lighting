986168f9dad0cde983454d3177a1e73ebde27981
David Brady <dbrady@shinybit.com>
2017-12-06 19:45:10 -0500

    Clean up message classes

--- lib/test_driven_lighting/messages/log_message.rb
+++ lib/test_driven_lighting/messages/log_message.rb
@@ -1,12 +1,14 @@
 require_relative 'message'
 
+# LogMessage - prints a message to the log (currently just whatever screen is
+# running the listener)
 class LogMessage < Message
-  def initialize(message)
+  def initialize message
     super
   end
 
   def dispatch
-    content = message.fetch("content")
+    content = message.fetch 'content'
     puts content
   end
 end
--- lib/test_driven_lighting/messages/message.rb
+++ lib/test_driven_lighting/messages/message.rb
@@ -1,3 +1,5 @@
+# Base Message class. Child classes should call super from #initialize and must
+# implement #process.
 class Message
   # (Mostly) Common message attributes
   attr_reader :message
@@ -6,11 +8,13 @@ class Message
   # MUST contain at least "message_type" => "name_of_this_message" so we know
   # which message class should handle it, though technically that's all handled
   # by MessageDispatcher before we ever construct the message
-  def initialize(message)
+  def initialize message
     @message = message
   end
 
-  def message_type; message.fetch("message_type"); end
+  def message_type
+    message.fetch 'message_type'
+  end
 
   # NOTE: Subclass is expected to implement #process
 end
