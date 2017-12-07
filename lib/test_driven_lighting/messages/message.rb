91d250665a8ea2efad4e352e6170c1f8c6c7b85d
David Brady <dbrady@shinybit.com>
2017-12-06 19:32:59 -0500

    Implement Message#message_type, clean up Message

--- lib/test_driven_lighting/messages/message.rb
+++ lib/test_driven_lighting/messages/message.rb
@@ -2,22 +2,15 @@ class Message
   # (Mostly) Common message attributes
   attr_reader :message
 
+  # message: hash with string keys containing the message data.
+  # MUST contain at least "message_type" => "name_of_this_message" so we know
+  # which message class should handle it, though technically that's all handled
+  # by MessageDispatcher before we ever construct the message
   def initialize(message)
     @message = message
   end
 
-  def message_type;  message.fetch["message_type"]; end
+  def message_type; message.fetch("message_type"); end
 
-  # message: hash with string keys containing the message data.
-  # MUST contain at least "message_type" => "name_of_this_message" so we know
-  # which message class should handle it
-  #
   # NOTE: Subclass is expected to implement #process
-  #
-  # I was going to raise NotImplementedError here but
-  # http://chrisstump.online/2016/03/23/stop-abusing-notimplementederror/ has
-  # convinced me to stop doing this entirely. TL;DR NotImplementedError means
-  # your PLATFORM does not support a feature of the language. Child classes in
-  # Ruby not implementing an abstract method should be handled by our good
-  # friend NoMethodError.
 end
--- spec/messages/message_spec.rb
+++ spec/messages/message_spec.rb
@@ -4,6 +4,12 @@ require 'messages/message.rb'
 describe Message do
   subject { Message.new 'message_type' => 'none' }
 
+  describe '#message_type' do
+    it 'returns the message type string' do
+      expect(subject.message_type).to eq 'none'
+    end
+  end
+
   describe '#process' do
     it 'is abstract and has no default implementation' do
       expect { subject.process }.to raise_error NoMethodError, /`process'/
